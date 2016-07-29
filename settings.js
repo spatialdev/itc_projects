940fa3a0c87ec26cf084a4f7a7250c50cafaa6a4
Daniel Baah <dbaah@spatialdev.com>
2016-07-29 16:27:56 -0700

    fake settings

---
+++ settings.js
@@ -0,0 +1,15 @@
+/**
+ * This is the settings file for
+ * the DB.
+ */
+
+module.exports = {
+    // Postgres Database Connection
+    //pg: {
+    //    server: 'localhost',
+    //    port: '5432',
+    //    database: 'db_name',
+    //    user: 'user',
+    //    password: 'password'
+    //},
+};
